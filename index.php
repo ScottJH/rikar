e32850630483d7fe323a151dfdcdd2fe23cee6a3
Scott Haselwood <scott.j.haselwood@gmail.com>
2014-05-20 15:22:19 -0700

    Initial Commits
    Added bootstrap files and setup dev environment

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap Lastest test</title>
+
+    <!-- Bootstrap -->
+    <link href="stylesheets/screen.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  	<div class="container">
+    	<h1>Hello, world!</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, tempora, voluptas est quae rerum voluptatum ab consequuntur doloribus nihil sapiente quos aperiam aliquam. Officia, corporis, magnam repellat officiis assumenda expedita?<br>
+			<button type="button" class="btn btn-primary">I'm a button </button></p>
+	</div>  
+    <!-- jQuqq  2  qqaw3qwq qqqq qery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled qplugins (below), or include individual files as needed -->
+    <script src="assets/javascrqq ipts/bootstrap.js"></script>
+  </body>
+</html>	
